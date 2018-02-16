e04533c4505a8b993cdc8c1912468e749b4e01bb
k3ndzy <36295378+k3ndzy@users.noreply.github.com>
2018-02-16 15:48:21 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "check",
+  "version": "1.0.0",
+  "description": "make by cen",
+  "main": "index.js",
+  "author": "Me",
+  "License": "ISC",
+  "dependencies": {
+    "discord.js": "^11.2.1"
+  }
+}
